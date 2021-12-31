1533380b349048f3536cd9423bf6e4ab35b13ef7
Sebastian <sebas.cj@gmail.com>
2021-12-31 11:13:56 -0500

    fixed breadcrumb for search page

--- apps/service/src/base/base.ts
+++ apps/service/src/base/base.ts
@@ -48,6 +48,26 @@ const getDescription = <T>(id: string): Promise<T> => {
   return getRequest(url);
 };
 
+const parseCategories = async (id: string): Promise<string[]> => {
+  let categoryData: { path_from_root: { name: string }[] };
+  let categories = [];
+  try {
+    categoryData = await getCategory(id);
+    if (categoryData && categoryData.path_from_root) {
+      categories = categoryData.path_from_root.map((path) => {
+        return path.name;
+      });
+    }
+    return categories;
+  } catch (e) {
+    return [];
+  }
+};
+
+const getCategoryFromFilters = (filters) => {
+  return filters.filter((filter) => filter.id === 'category')[0];
+};
+
 const getItemsList = (query: string, limit: string): Promise<List> => {
   return new Promise((resolve, reject) => {
     if (!query) {
@@ -58,13 +78,29 @@ const getItemsList = (query: string, limit: string): Promise<List> => {
       url = `${url}&limit=${limit}`;
     }
     getRequest(url)
-      .then((data: any) => {
-        const categoryFilters = data.filters.filter(
-          (filter) => filter.id === 'category'
-        )[0];
-        const categories: string[] =
+      .then(async (data: any) => {
+        let categoryFilters = getCategoryFromFilters(data.available_filters);
+        if (!categoryFilters) {
+          categoryFilters = getCategoryFromFilters(data.filters);
+        }
+        const mostRelevant =
           categoryFilters &&
-          categoryFilters.values[0].path_from_root.map((type) => type.name);
+          categoryFilters.values.reduce(
+            (higherResult, category) => {
+              if (
+                category.results > higherResult.results ||
+                category.path_from_root
+              ) {
+                higherResult = category;
+              }
+              return higherResult;
+            },
+            {
+              results: 0,
+            }
+          );
+
+        const categories = await parseCategories(mostRelevant.id);
         const items: Item[] = data.results.map(
           ({
             id,
@@ -112,7 +148,7 @@ const getItemDetail = (id: string): Promise<Details> => {
     let item: Item;
     getRequest(url)
       .then(
-        ({
+        async ({
           id,
           category_id,
           title,
@@ -124,6 +160,7 @@ const getItemDetail = (id: string): Promise<Details> => {
           sold_quantity,
         }) => {
           const parsedPrice = parsePrice(price);
+          categories = await parseCategories(category_id);
           item = {
             id,
             title,
@@ -137,17 +174,9 @@ const getItemDetail = (id: string): Promise<Details> => {
             },
             sold_quantity,
           };
-          return getCategory(category_id);
+          return getDescription(id);
         }
       )
-      .then((categoryData: { path_from_root: { name: string }[] }) => {
-        if (categoryData && categoryData.path_from_root) {
-          categories = categoryData.path_from_root.map((path) => {
-            return path.name;
-          });
-        }
-        return getDescription(id);
-      })
       .then((descriptionData: { plain_text: string }) => {
         item.description = descriptionData.plain_text;
         const details: Details = {
